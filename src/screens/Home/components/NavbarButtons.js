0c201e4d7997a86054dd8cf493b5e5351464bbd3
Endeavour-Innovations-Inc <albert.lipaev@wsu.edu>
2023-07-02 23:36:10 -0700

    Cleaned a lot of .css

--- src/screens/Home/components/NavbarButtons.js
+++ src/screens/Home/components/NavbarButtons.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Home } from '../Home';
 
 const NavbarButtons = () => {
 
