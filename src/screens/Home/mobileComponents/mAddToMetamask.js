1045863dcc32801421a8547db7a2f59b774315eb
Endeavour-Innovations-Inc <albert.lipaev@wsu.edu>
2023-07-06 18:25:12 -0700

    Mobile version adding token potential fix

--- src/screens/Home/mobileComponents/mAddToMetamask.js
+++ src/screens/Home/mobileComponents/mAddToMetamask.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { LiArrowUpRight2 } from '../../../icons/LiArrowUpRight2';
 
 const MAddToMetamask = () => {
-
   const textStyle = {
     color: '#ffffff',
     fontFamily: '"TT Firs Neue-Medium", Helvetica',
@@ -16,30 +15,27 @@ const MAddToMetamask = () => {
 
   const addToMetamask = async () => {
     try {
-      // Check if MetaMask is installed
       if (!window.ethereum) {
         alert('MetaMask is not installed.');
         return;
       }
-  
-      // Log the network for debugging
+
       const network = await window.ethereum.request({ method: 'net_version' });
       console.log(`MetaMask Network ID: ${network}`);
-  
-      // Try to add the token
+
       const wasAdded = await window.ethereum.request({
         method: 'wallet_watchAsset',
         params: {
           type: 'ERC20',
           options: {
-            address: '0x77D00901BA98F11d8EB87779C5ad69ee3240CB12', // Token Contract Address
-            symbol: 'BOOM', // Token Symbol
-            decimals: 18, // Token Decimals
-            image: 'https://i.imgur.com/4hLOhhO.png', // Direct link to the token image
+            address: '0x77D00901BA98F11d8EB87779C5ad69ee3240CB12',
+            symbol: 'BOOM',
+            decimals: 18,
+            image: 'https://i.imgur.com/4hLOhhO.png',
           },
         },
       });
-  
+
       if (wasAdded) {
         console.log('Token was successfully added to MetaMask');
       } else {
@@ -51,10 +47,10 @@ const MAddToMetamask = () => {
   };
 
   return (
-    <button className="button" onClick={addToMetamask}>
+    <div className="button" onClick={addToMetamask}>
       <div style={textStyle}>Add to MetaMask</div>
       <LiArrowUpRight2 style={{fill: 'white'}} />
-    </button>
+    </div>
   );
 };
 
