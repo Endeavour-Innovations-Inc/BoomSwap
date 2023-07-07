c4fda77fb8afc1ead5ce47e1628e9673217b527e
Endeavour-Innovations-Inc <albert.lipaev@wsu.edu>
2023-07-06 18:28:33 -0700

    Mobile version returned the original button

--- src/screens/Home/mobileComponents/mAddToMetamask.js
+++ src/screens/Home/mobileComponents/mAddToMetamask.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { LiArrowUpRight2 } from '../../../icons/LiArrowUpRight2';
 
 const MAddToMetamask = () => {
+
   const textStyle = {
     color: '#ffffff',
     fontFamily: '"TT Firs Neue-Medium", Helvetica',
@@ -15,27 +16,30 @@ const MAddToMetamask = () => {
 
   const addToMetamask = async () => {
     try {
+      // Check if MetaMask is installed
       if (!window.ethereum) {
         alert('MetaMask is not installed.');
         return;
       }
-
+  
+      // Log the network for debugging
       const network = await window.ethereum.request({ method: 'net_version' });
       console.log(`MetaMask Network ID: ${network}`);
-
+  
+      // Try to add the token
       const wasAdded = await window.ethereum.request({
         method: 'wallet_watchAsset',
         params: {
           type: 'ERC20',
           options: {
-            address: '0x77D00901BA98F11d8EB87779C5ad69ee3240CB12',
-            symbol: 'BOOM',
-            decimals: 18,
-            image: 'https://i.imgur.com/4hLOhhO.png',
+            address: '0x77D00901BA98F11d8EB87779C5ad69ee3240CB12', // Token Contract Address
+            symbol: 'BOOM', // Token Symbol
+            decimals: 18, // Token Decimals
+            image: 'https://i.imgur.com/4hLOhhO.png', // Direct link to the token image
           },
         },
       });
-
+  
       if (wasAdded) {
         console.log('Token was successfully added to MetaMask');
       } else {
@@ -47,10 +51,10 @@ const MAddToMetamask = () => {
   };
 
   return (
-    <div className="button" onClick={addToMetamask}>
+    <button className="button" onClick={addToMetamask}>
       <div style={textStyle}>Add to MetaMask</div>
       <LiArrowUpRight2 style={{fill: 'white'}} />
-    </div>
+    </button>
   );
 };
 
