db785ada2e2298e57cddbc641641eeeeca8f3193
Endeavour-Innovations-Inc <albert.lipaev@wsu.edu>
2023-08-11 17:40:03 -0700

    the rest of update

--- src/screens/Home/components/NavbarButtons.js
+++ src/screens/Home/components/NavbarButtons.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Home } from '../Home';
 
 const NavbarButtons = () => {
 
